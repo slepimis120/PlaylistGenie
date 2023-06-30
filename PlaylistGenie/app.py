fe3f80cc431f9adfa9400cbc704c96307b2dac10
unknown <slepimis120@gmail.com>
2023-06-30 21:30:18 +0200

    Small fixes with connecting HTML and CSS

--- PlaylistGenie/app.py
+++ PlaylistGenie/app.py
@@ -4,8 +4,12 @@ import PlaylistGenie.templates
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def main():
     return render_template('main.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(host="localhost", port=3000)
