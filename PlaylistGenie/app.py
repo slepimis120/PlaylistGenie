d6ddaf77a8849fee11d86975a098fdb060bd49c5
sv5020 <99079887+sv50-2020@users.noreply.github.com>
2023-07-02 13:11:47 +0200

    fixed bugs

--- PlaylistGenie/app.py
+++ PlaylistGenie/app.py
@@ -1,22 +1,91 @@
-from flask import Flask, render_template
-import methods.spotify as spotify
+import base64
+import time
+import urllib
+
+import spotipy
+from flask import Flask, render_template, redirect, request, session, url_for
 import PlaylistGenie.methods.spotify as spotify
 
 app = Flask(__name__)
+
+
+app.secret_key = 'd53bda4f89b0b448759c66e1c9'
+app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
+
+client_id = "f491929c43934c9487efb95c557b55c9"
+client_secret = "d34016d53bda4f89b0b448759c66e1c9"
+redirect_uri = "http://localhost:3000"
+scope = "playlist-read-private"
 
 
 @app.route("/")
 def index():
     return render_template("main.html")
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 @app.route('/login')
 def login():
-    spotify.authorize_user()
-    return render_template('about.html')
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+
+@app.route('/authorize')
+def authorize():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info
+    return redirect("/getPlaylists")
+
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
+
+
+@app.route('/getPlaylists')
+def get_user_playlists():
+    session['token_info'], authorized = get_token()
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    print(session.get('token_info').get('access_token'))
+    my_playlists = sp.current_user_playlists(limit=50)['items']
+    print(my_playlists)
+    return render_template("home.html", playlists=my_playlists)
+
+
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    if is_token_expired:
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
+
+
+def create_spotify_oauth():
+    return spotipy.SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=url_for('authorize', _external=True),
+        scope=scope)
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=3000)
